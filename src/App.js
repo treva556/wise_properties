8f3ce8a5a2bdcab984d3e30480872bc75f4ceebd
treva556 <marktreva92@gmail.com>
2024-07-09 00:58:42 +0300

    start

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className=' items-center p-4'>
-        <p className=' text-3xl'>  Happy Coding! </p>
-
+      <div className="relative items-center p-4 bg-amber-100 h-16">
+        <p className="absolute right-0 text-3xl">Happy Coding!</p>
       </div>
+
       <div className="grid grid-cols-3 gap-5 ml-5 mr-5">
         <div className="item col-span-1 bg-blue-200 p-20 items-center">
           <p className=' text-center text-4xl font-bold text-red-700'> HAPPY CODING!</p>
@@ -17,7 +17,13 @@ function App() {
         <div className="item col-span-1 bg-blue-400 p-4">Item 3</div>
        
       </div>
+...
 
+      <div className=' items-end'>
+        <p className=' items-end'>first</p>
+        <p>mid</p>
+        <p>end</p>
+      </div>
       <div className="container mx-auto p-4">
       <div className="grid grid-rows-5 gap-5">
         <div className="row-span-1 bg-green-200 p-4">Item 1</div>
