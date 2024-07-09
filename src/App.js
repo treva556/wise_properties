a620b14ef17ba6b7400a7c6d2913860df6fbb671
treva556 <marktreva92@gmail.com>
2024-07-10 00:19:12 +0300

    start

--- src/App.js
+++ src/App.js
@@ -4,52 +4,34 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className="relative items-center p-4 bg-amber-100 h-16">
-        <p className="absolute right-0 text-3xl">Happy Coding!</p>
-      </div>
-
-      <div className="grid grid-cols-3 gap-5 ml-5 mr-5">
-        <div className="item col-span-1 bg-blue-200 p-20 items-center">
-          <p className=' text-center text-4xl font-bold text-red-700'> HAPPY CODING!</p>
+    <div className='bg-custom-cream'>
+      <nav className='flex justify-around p-4'>
+        <a href='#' className='text-black'>Services</a>
+        <a href='#' className='text-black'>Contacts</a>
+        <a href='#' className='text-black'>About Us</a>
+      </nav>
+      <div className='text-center mt-10'>
+        <h1 className='text-5xl'>BUILD WITH US</h1>
+        <p className='mt-4'>World's number one property developer</p>
+        <div className='flex justify-around mt-10'>
+          <div className='bg-custom-blue p-10'>Hotels</div>
+          <div className='bg-custom-pink p-10'>Apartments</div>
+          <div className='bg-custom-yellow p-10'>Mansions</div>
         </div>
-        <div className="item col-span-1 bg-blue-300 p-20">Item 2</div>
-        <div className="item col-span-1 bg-blue-400 p-4">Item 3</div>
-       
+        <div className='mt-10'>
+          <div className='bg-custom-blue p-10'>Why Choose Us?</div>
+          <div className='bg-custom-pink p-10'>Our Client Reviews</div>
+          <div className='bg-custom-yellow p-10'>Things I'll add later</div>
+        </div>
+        <div className='bg-custom-cream p-10'>Things I'll add later</div>
+        <footer className='bg-custom-dark-green p-4 text-center text-white'>
+          FOOTER
+        </footer>
       </div>
-...
-
-      <div className=' items-end'>
-        <p className=' items-end'>first</p>
-        <p>mid</p>
-        <p>end</p>
-      </div>
-      <div className="container mx-auto p-4">
-      <div className="grid grid-rows-5 gap-5">
-        <div className="row-span-1 bg-green-200 p-4">Item 1</div>
-        <div className="row-span-2 bg-green-300 p-4">Item 2</div>
-        <div className="row-span-1 bg-green-400 p-4">Item 3</div>
-        <div className="row-span-1 bg-green-500 p-4">Item 4</div>
-        <div className="row-span-1 bg-green-600 p-4">Item 5</div>
-      </div>
-
-
-    </div>
-
-    <div className="container mx-auto p-4">
-      <div className="grid grid-cols-3 gap-5">
-        <div className="item col-span-1 bg-blue-200 p-4">Item 1</div>
-        <div className="item col-span-2 bg-blue-300 p-4">Item 2</div>
-        <div className="item col-span-1 bg-blue-400 p-4">Item 3</div>
-        <div className="item col-span-1 bg-blue-500 p-4">Item 4</div>
-        <div className="item col-span-1 bg-blue-600 p-4">Item 5</div>
-      </div>
-    </div>
-
     </div>
   );
 }
 
 export default App;
 
-
+////
