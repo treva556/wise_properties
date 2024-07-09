ebf6f25df2d7ce5e7d947579322a889b2d411ec4
treva556 <marktreva92@gmail.com>
2024-07-10 02:26:04 +0300

    start

--- src/App.js
+++ src/App.js
@@ -2,49 +2,42 @@
 import React from 'react';
 import './App.css';
 
-
 function App() {
   return (
     <div className='bg1'>
-      <nav className='flex justify-end p-4 space-x-16 bg-gray-800'>
+      <nav className='flex justify-end p-4 space-x-4 md:space-x-16 bg-gray-800'>
         <a href='#' className='c1 font-mono'>Services</a>
-        <a href='#' className=' c1'>Contacts</a>
-        <a href='#' className=' c1'>About Us</a>
+        <a href='#' className='c1'>Contacts</a>
+        <a href='#' className='c1'>About Us</a>
       </nav>
 
-
       <div className='text-center mt-10'>
-        <div className=' p-20'>
-        <h1 className='text-5xl'>BUILD WITH US</h1>
-        <p className='mt-4'>World's number one property developer</p>
+        <div className='p-5 md:p-20'>
+          <h1 className='text-3xl md:text-5xl'>BUILD WITH US</h1>
+          <p className='mt-4'>World's number one property developer</p>
         </div>
-        <div className='flex justify-around mt-10'>
-          <div className=' bg-blue-400 p-5 rounded text-sm'>Hotels
-            <p> We at ser have harious hotels ,
-              
-            </p>
+        <div className='flex flex-col md:flex-row justify-around mt-10'>
+          <div className='bg-blue-400 p-5 rounded text-sm m-2 md:m-0 w-full md:w-auto'>
+            Hotels
+            <p> We at ser have various hotels,</p>
             <p> click below for more</p>
           </div>
-          <div className=' bg-pink-300 p-2 rounded-2xl'>Apartments</div>
-          <div className=' bg-yellow-300 p-2'>Mansions</div>
+          <div className='bg-pink-300 p-5 rounded-2xl m-2 md:m-0 w-full md:w-auto'>Apartments</div>
+          <div className='bg-yellow-300 p-5 m-2 md:m-0 w-full md:w-auto'>Mansions</div>
         </div>
-        <div className=' bg-green-200'>
-        <div className='flex justify-around mt-10'>
-
-          <div className=' p-32'>Why Choose Us?</div>
-          <div className='bg-custom-pink p-10'>Add later</div>
+        <div className='bg-green-200'>
+          <div className='flex flex-col md:flex-row justify-around mt-10'>
+            <div className='p-10 md:p-32'>Why Choose Us?</div>
+            <div className='bg-custom-pink p-5 md:p-10'>Add later</div>
+          </div>
         </div>
-
+        <div className='flex flex-col md:flex-row justify-around mt-10'>
+          <div className='bg-custom-pink p-5 md:p-10'>Our Client Reviews</div>
+          <div className='bg-custom-cream p-5 md:p-10'>Things I'll add later</div>
         </div>
-        <div className='flex justify-around mt-10'>
-        <div className='bg-custom-pink p-10'>Our Client Reviews</div>
-
-        <div className='bg-custom-cream p-10'>Things I'll add later</div>
-        </div>
-        <footer className=' h-32 bg-gray-800 p-4 text-center text-white'>
+        <footer className='h-32 bg-gray-800 p-4 text-center text-white'>
           FOOTER
         </footer>
-      
       </div>
     </div>
   );
