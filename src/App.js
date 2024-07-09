9d1846845e46b959eec9865cf966fabf9adbe06c
treva556 <marktreva92@gmail.com>
2024-07-10 02:11:05 +0300

    start

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import './App.css';
 
+
 function App() {
   return (
     <div className='bg1'>
@@ -10,20 +11,25 @@ function App() {
         <a href='#' className=' c1'>Contacts</a>
         <a href='#' className=' c1'>About Us</a>
       </nav>
+
+
       <div className='text-center mt-10'>
         <div className=' p-36'>
         <h1 className='text-5xl'>BUILD WITH US</h1>
         <p className='mt-4'>World's number one property developer</p>
         </div>
         <div className='flex justify-around mt-10'>
-          <div className=' bg-blue-400 p-10'>Hotels</div>
+          <div className=' bg-blue-400 p-20'>Hotels</div>
           <div className='bg-custom-pink p-10'>Apartments</div>
           <div className='bg-custom-yellow p-10'>Mansions</div>
         </div>
+        <div className=' bg-green-200'>
         <div className='flex justify-around mt-10'>
 
-          <div className='bg-custom-blue p-10'>Why Choose Us?</div>
+          <div className=' p-32'>Why Choose Us?</div>
           <div className='bg-custom-pink p-10'>Add later</div>
+        </div>
+
         </div>
         <div className='flex justify-around mt-10'>
         <div className='bg-custom-pink p-10'>Our Client Reviews</div>
