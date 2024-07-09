78ca3d8352c5762fdf4377413cd4b28b0af628bc
treva556 <marktreva92@gmail.com>
2024-07-10 02:33:51 +0300

    start

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           <h1 className='text-3xl md:text-5xl'>BUILD WITH US</h1>
           <p className='mt-4'>World's number one property developer</p>
         </div>
-        <div className='flex flex-col md:flex-row justify-around mt-10'>
+        <div className='flex flex-col md:flex-row justify-around mt-10 mr-6'>
           <div className='bg-blue-400 p-5 rounded text-sm m-2 md:m-0 w-full md:w-auto'>
             Hotels
             <p> We at ser have various hotels,</p>
@@ -25,8 +25,9 @@ function App() {
           <div className='bg-pink-300 p-5 rounded m-2 md:m-0 w-full md:w-auto'>Apartments</div>
           <div className='bg-yellow-300 p-5 m-2 md:m-0 w-full md:w-auto rounded'>Mansions</div>
         </div>
+        <p>hh</p>
         <div className='bg-green-200'>
-          <div className='flex flex-col md:flex-row justify-around mt-10'>
+          <div className='flex flex-col md:flex-row justify-around mt-5'>
             <div className='p-10 md:p-32 text-2xl font-bold'>Why Choose Us?</div>
             <div className='bg-custom-pink p-5 md:p-10'>
               <p> Here at Ser, we are dedicated to find and give everyone their dream properties</p>
