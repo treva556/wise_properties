c5867228c112c7c155749f9956435616871cdd1c
treva556 <marktreva92@gmail.com>
2024-08-05 19:53:40 +0300

    mile

--- src/App.js
+++ src/App.js
@@ -26,29 +26,7 @@ function App() {
           <div className='bg-pink-300 p-5 rounded m-2 md:m-0 w-full md:w-auto shadow-xl'>Apartments</div>
           <div className='bg-yellow-300 p-5 m-2 md:m-0 w-full md:w-auto rounded'>Mansions</div>
         </div>
-        <p>hh</p>
-        <div className='bg-green-200'>
-          <div className='flex flex-col md:flex-row justify-around mt-5'>
-            <div className='p-10 md:p-32 text-2xl font-bold'>Why Choose Us?</div>
-            <div className='bg-custom-pink p-5 md:p-10'>
-              <p> Here at Ser, we are dedicated to build our clients their dream properties</p>
-            </div>
-          </div>
-        </div>
-
-        <div className='bg-green-200'>
-          <div className='flex flex-col md:flex-row justify-around mt-5'>
-            <div className='p-5 md:p-16 text-2xl font-bold'>Our Projects</div>
-            <div className='bg-custom-pink p-3 md:p-5'>
-              <p> Link</p>
-            </div>
-          </div>
-        </div>
-
-        <div className='flex flex-col md:flex-row justify-around mt-4'>
-          <div className='bg-custom-pink p-5 md:p-10'>Our Client Reviews</div>
-          <div className='bg-custom-cream p-5 md:p-10'>Things I'll add later</div>
-        </div>
+        
         <footer className=' columns-2 h-32 bg-gray-800 p-4 text-center text-white'>
           <div>
           FOOTER
