9094b15ab5ec76b383fd194a3090801e2ca171a4
treva556 <marktreva92@gmail.com>
2024-08-12 23:12:08 +0300

    mile

--- src/App.js
+++ src/App.js
@@ -17,6 +17,17 @@ function App() {
           <h1 className='text-3xl md:text-5xl p-8'>BUILD WITH US</h1>
           <p className='mt-4'>World's number one property developer</p>
         </div>
+        <div>
+          <h2>About Us </h2>
+        </div>
+        <div>
+          <h2> What we Do</h2>
+          <p>  Property management
+          </p>
+          <p> Buying,remodelling & reselling estates </p>
+          <p> Building modern structures </p>
+
+        </div>
         <div className='flex flex-col md:flex-row justify-around mt-10 mr-6 p-4'>
           <div className='bg-blue-400 p-5 rounded text-sm m-2 md:m-0 w-full md:w-auto'>
             Hotels
@@ -26,13 +37,27 @@ function App() {
 
           <div className='bg-yellow-300 p-5 m-2 md:m-0 w-full md:w-auto rounded'>Other</div>
         </div>
+        <div>
+          <h2>Mission</h2>
+        </div>
+        <div>
+          <h2> Assets numbers</h2>
+        </div>
+        <div>
+          <h2> Top Management </h2>
+        </div>
+        <div>
+          <h2> Top Clients </h2>
+        </div>
         
+
         <footer className=' columns-2 h-32 bg-gray-800 p-4 text-center text-white'>
           <div>
           FOOTER
           </div>
           <div>
-          FOOTER//
+          <h6> FOOTER </h6>
+            <p> contacts & headquaters</p>
           </div>
           
         </footer>
